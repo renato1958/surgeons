671dadced121c86c58fbb20b2f882e102d003279
renato1958 <felisi.renato@gmail.com>
2021-09-18 19:37:14 +0200

    Aggiunti un titolo e una foto

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <surgeon></surgeon>
 </template>
 
 <script>
+import surgeon from './components/Surgeon';
 
 export default {
   name: 'App',
   components: {
+    surgeon
   }
 }
 </script>
---
+++ src/components/Surgeon.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Pinco Pallo</h1>
+    <img src="../assets/img/pinco-pallo.jpg" alt="Pinco Pallo">
+</template>
+
+<script>
+    export default {
+        name: 'surgeon'
+    }
+</script>
